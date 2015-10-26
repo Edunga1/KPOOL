fbcf8bcc4b3c516b0e882531fb9d376bf30e2ca9
Edunga1 <gkfkr21@naver.com>
2015-10-26 22:11:39 +0900

    실사용 환경

--- KumohCarPoolStatic/WebContent/js/kcp_carpool.js
+++ KumohCarPoolStatic/WebContent/js/kcp_carpool.js
@@ -9,7 +9,8 @@
 // 카풀 서버와 디바이스 정보
 var KCP = {
 	domain:				"http://192.168.218.201:8080/KumohCarPool",	// 서버 URL
-	deviceid:			"zb",									// device id
+	// deviceid:			"a",									// device id for test
+	deviceid:			null,									// device id
 	regid:				null,									// google gcm regid
 };
 
@@ -19,7 +20,8 @@ document.addEventListener("backbutton", function (e){
 }, false);
 
 // device 정보 획득 후 angularjs 부트스트랩
-document.addEventListener("DOMContentLoaded", function(){
+// document.addEventListener("DOMContentLoaded", function(){	// for test
+document.addEventListener("deviceready", function(){
 
 	// 푸쉬 Receive / Regist Callback function - ANDROID
 	// onNotification***은 반드시 window의 멤버함수로 존재해야 함
